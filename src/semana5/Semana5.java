914542197a6ed2fde72dd0d2615f57bb1be5f88e
CristhoferVaquezVera <vcristhofer08142@gmail.com>
2026-05-03 21:51:21 -0500

    Modificación de Semana5 (validaciones)

--- src/semana5/Semana5.java
+++ src/semana5/Semana5.java
@@ -35,20 +35,37 @@ public class Semana5 {
           switch(op){
               
               case 1:
-                  try
-                  {
-                      System.out.println("Ingrese el Codigo");
-                        int Codigo = sc.nextInt();
-                      System.out.println("Ingrese el Nombre");
-                        String Nombre = sc.next();
-                      System.out.println("Ingrese su edad");
-                        int edad = sc.nextInt();
-                      alumno p1 = new alumno(Codigo,Nombre,edad);
-                        c.AsignarEstudiante(p1);
-                  }catch(InputMismatchException e){
-                  System.out.println("Error: Rellene bien los campos "+ e);
-                  sc.next();
-                  }
+                 // VALIDACIONES  (aqui hubo cambios)
+                   String Codigo;
+                   while (true) {
+                   System.out.println("Ingrese el Código (8 dígitos): ");
+                   Codigo = sc.next();
+
+                   if (Codigo.length() == 8 && Codigo.matches("\\d+")) {
+                   break;
+                   } else {System.out.println("Error: el código debe tener 8 dígitos numéricos"); }}
+
+                   String Nombre;
+                   while (true) {
+                   System.out.println("Ingrese el Nombre: ");
+                   Nombre = sc.next();
+
+                   if (Nombre.matches("[a-zA-Z]+")) {
+                   break;
+                   } else {System.out.println("Error: el nombre solo debe tener letras");} }
+
+                  int edad;
+                  while (true) {
+                  System.out.println("Ingrese la edad: ");
+                  edad = sc.nextInt();
+
+                  if (edad > 0) {
+                  break;
+                  } else {System.out.println("Error: la edad debe ser mayor a 0"); }}
+
+                 // se crea un objeto y se agrega a la lista 
+                 alumno p1 = new alumno(Integer.parseInt(Codigo), Nombre, edad);
+                 c.AsignarEstudiante(p1);
                   break;
               case 2:
                   System.out.println(c.ListadoEstudiantes());
